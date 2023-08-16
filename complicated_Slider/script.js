9daabd7b5c94d6de6eb7220d74db8889fca964d9
Imran Rafiq Rather <emmeiwhite@gmail.com>
2023-08-16 05:38:08 +0530

    Complicated Slider also done

--- complicated_Slider/script.js
+++ complicated_Slider/script.js
@@ -1,6 +1,21 @@
-const slides = document.querySelectorAll(".slide");
+const slider = document.querySelector(".slider");
 const prevButton = document.getElementById("prevButton");
 const nextButton = document.getElementById("nextButton");
+const cards = document.querySelectorAll(".card");
+const cardsPerSlide = 6;
+
+// Wrap the cards in slide divs
+for (let i = 0; i < cards.length; i += cardsPerSlide) {
+  const slide = document.createElement("div");
+  slide.classList.add("slide");
+  for (let j = i; j < i + cardsPerSlide && j < cards.length; j++) {
+    slide.appendChild(cards[j]);
+  }
+  slider.appendChild(slide);
+}
+
+// Once the wrappers are added, we can we rest assured about slider functionality, which we already completed.
+const slides = document.querySelectorAll(".slide");
 
 slides.forEach((slide, index) => {
   slide.style.left = `${index * 100}%`;
